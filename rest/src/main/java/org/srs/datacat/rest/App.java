f55338645982d839e5ea08ade061c4e66f7a8fff
Brian Van Klaveren <bvan@slac.stanford.edu>
2014-09-22 18:49:35 +0000

    Make a wrapper for dataSource that sets all autocommit to false.

--- rest/src/main/java/org/srs/datacat/rest/App.java
+++ rest/src/main/java/org/srs/datacat/rest/App.java
@@ -5,9 +5,12 @@ import org.srs.datacat.model.RequestAcceptFilter;
 import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 import java.io.File;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.URL;
 import java.net.URLClassLoader;
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.SQLFeatureNotSupportedException;
 import java.util.Iterator;
 import java.util.ServiceLoader;
 import java.util.logging.Level;
@@ -106,7 +109,59 @@ public class App extends ResourceConfig {
             javax.naming.Context ctx = new InitialContext();
             // relative to standard JNDI root for J2EE app
             javax.naming.Context envCtx = (javax.naming.Context) ctx.lookup( "java:comp/env" );
-            return (DataSource) envCtx.lookup( jndi );
+            final DataSource ds = (DataSource) envCtx.lookup( jndi );
+            return new DataSource(){
+
+                @Override
+                public Connection getConnection() throws SQLException{
+                    Connection conn = ds.getConnection();
+                    conn.setAutoCommit(false);
+                    return conn;
+                }
+
+                @Override
+                public Connection getConnection(String username, String password) throws SQLException{
+                    Connection conn = ds.getConnection( username, password );
+                    conn.setAutoCommit(false);
+                    return conn;
+                }
+
+                @Override
+                public PrintWriter getLogWriter() throws SQLException{
+                    return ds.getLogWriter();
+                }
+
+                @Override
+                public void setLogWriter(PrintWriter out) throws SQLException{
+                    ds.setLogWriter( out );
+                }
+
+                @Override
+                public void setLoginTimeout(int seconds) throws SQLException{
+                    ds.setLoginTimeout( seconds );
+                }
+
+                @Override
+                public int getLoginTimeout() throws SQLException{
+                    return ds.getLoginTimeout();
+                }
+
+                @Override
+                public Logger getParentLogger() throws SQLFeatureNotSupportedException{
+                    return ds.getParentLogger();
+                }
+
+                @Override
+                public <T> T unwrap(Class<T> iface) throws SQLException{
+                    return ds.unwrap( iface );
+                }
+
+                @Override
+                public boolean isWrapperFor( Class<?> iface) throws SQLException{
+                    return ds.isWrapperFor( iface );
+                }
+                
+            };
         } catch(NamingException ex) {
             throw new IllegalStateException(ex);
         }
