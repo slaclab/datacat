74c950bb5ef27c8eec3ce4c9024844dabf8ae30c
Brian Van Klaveren <bvan@slac.stanford.edu>
2014-09-30 19:12:11 +0000

    Add setup.py file

---
+++ client/python/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+requires = [
+    'requests>=2.3.0'
+]
+
+setup(
+    name='datacat',
+    version='0.1-ALPHA',
+    packages=['datacat'],
+    url='http://srs.slac.stanford.edu',
+    license='',
+    author='Brian Van Klaveren',
+    author_email='bvan@slac.stanford.edu',
+    description='Data Catalog RESTful client library',
+    zip_safe=False,
+    install_requires=requires,
+    entry_points={
+        'console_scripts': ['datacat = datacat.cli:main'],
+    }
+)
