7f4f22fc06d02f7ae6ac6080ff6f85d6b72e67b6
Brian Van Klaveren <bvan@slac.stanford.edu>
2014-09-30 19:30:32 +0000

    Move example around a bit. Modify imports for __init__.py

---
+++ client/python/examples/example.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+"""
+Make sure you have your environment setup!
+1. You need Python requests >= 2.3.0
+2. You need the datacat directory in your path.
+"""
+
+from datacat import Client, unpack
+from datacat.config import BASE_URL
+
+client = Client(BASE_URL("lsst", mode="dev"))
+
+path_pattern = "/EXO/Data/Raw/*"                     # Search containers under Raw
+query = 'nRun gt 6200 and exo.runQuality =~ "GO%"'   # Filter query
+sort = ["nRun-", "nEvents"]                          # Sort nRun desc, nEvents asc (asc default). These are also retrieved.
+show = ["nVetoEvents"]                               # Retrieve nVetoEvents as well.
+
+resp = client.search(path_pattern, query=query, sort=sort, show=show)
+
+if resp.status_code == 200:
+    for raw_dataset in resp.json():
+        dataset = unpack(raw_dataset)
+        print(dataset.fileSystemPath)
+        print("\t" + str(dataset.metadata))
+else:
+    print("Error processing request:" + str(resp.status_code))
+
