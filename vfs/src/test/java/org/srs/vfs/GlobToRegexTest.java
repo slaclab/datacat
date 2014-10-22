ef58afb47fe66a43cff353c9e577f788345fec67
Brian Van Klaveren <bvan@slac.stanford.edu>
2014-10-22 18:59:29 +0000

    Fix headers

--- vfs/src/test/java/org/srs/vfs/GlobToRegexTest.java
+++ vfs/src/test/java/org/srs/vfs/GlobToRegexTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 
 package org.srs.vfs;
 
@@ -12,7 +7,7 @@ import org.junit.Test;
 
 /**
  *
- * @author Brian Van Klaveren<bvan@slac.stanford.edu>
+ * @author bvan
  */
 public class GlobToRegexTest {
     
