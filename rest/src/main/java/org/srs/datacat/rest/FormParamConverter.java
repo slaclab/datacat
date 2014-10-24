db17bf1fc5b83a4391854a30fa76e625419183ec
Brian Van Klaveren <bvan@slac.stanford.edu>
2014-10-24 18:52:37 +0000

    Cleanup a few methods pertaining to Versions and the Builder Implement metadata extraction in FormParamConverter Implement Dataset building in FormParamConverter Add FormParamConverterTest Enforce location ordering in VersionWithLocations

--- rest/src/main/java/org/srs/datacat/rest/FormParamConverter.java
+++ rest/src/main/java/org/srs/datacat/rest/FormParamConverter.java
@@ -2,15 +2,29 @@
 package org.srs.datacat.rest;
 
 import com.fasterxml.jackson.annotation.JsonSetter;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import java.io.IOException;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.HashMap;
+import java.util.Iterator;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import javax.ws.rs.core.MultivaluedMap;
 import org.srs.datacat.model.DatasetContainer;
 import org.srs.datacat.shared.DatacatObject;
-import org.srs.datacat.shared.dataset.FlatDataset;
+import org.srs.datacat.shared.Dataset;
+import org.srs.datacat.shared.DatasetLocation;
+import org.srs.rest.shared.metadata.MetadataEntry;
 
 /**
  *
@@ -19,14 +33,22 @@ import org.srs.datacat.shared.dataset.FlatDataset;
 public class FormParamConverter {
 
     final static HashMap<String, Method> containerSetters = new HashMap<>();
-    final static HashMap<String, Method> flatDatasetSetters = new HashMap<>();
+    final static HashMap<String, Method> datasetSetters = new HashMap<>();
+    final static HashMap<String, Method> locationSetters = new HashMap<>();
 
     static {
         
-        FlatDataset.Builder dsbuilder = new FlatDataset.Builder();
-        for(Method m: dsbuilder.getClass().getMethods()){
+        Dataset.Builder dsBuilder = new Dataset.Builder();
+        for(Method m: dsBuilder.getClass().getMethods()){
             if(m.getAnnotation( JsonSetter.class ) != null){
-                containerSetters.put( m.getName(), m );
+                datasetSetters.put( m.getName(), m );
+            }
+        }
+        
+        DatasetLocation.Builder dslBuilder = new DatasetLocation.Builder();
+        for(Method m: dslBuilder.getClass().getMethods()){
+            if(m.getAnnotation( JsonSetter.class ) != null){
+                locationSetters.put( m.getName(), m );
             }
         }
         
@@ -46,28 +68,95 @@ public class FormParamConverter {
         buildFromParams(mapper, builder, containerSetters, formParams);
         return builder;
     }
-
-    public static FlatDataset.Builder getDatasetBuilder(MultivaluedMap<String,String> formParams){
-        FlatDataset.Builder builder = new FlatDataset.Builder();
+    
+    public static Dataset.Builder getDatasetBuilder(Map<String,List<String>> formParams){
+        HashMap<String, List<String>> datasetInfo = new HashMap<>();
+        HashMap<String,List<String>> locationInfo = new LinkedHashMap<>();
+        List<String> formParamKeys = new ArrayList<>(formParams.keySet());
+        Dataset.Builder builder = new Dataset.Builder();
+        // Sort it so priority locations added to locationInfo first
+        Collections.sort( formParamKeys );  
+        for(String name: formParamKeys){
+            if(name.startsWith("location")){
+                locationInfo.put( name, formParams.get( name ));
+            } else if(name.equals("versionMetadata")){
+                builder.versionMetadata(processMetadata(formParams.get(name)));
+            } else {
+                datasetInfo.put(name, formParams.get( name));
+            }
+        }
         ObjectMapper mapper = new ObjectMapper();
-        buildFromParams(mapper, builder, flatDatasetSetters, formParams);
+        
+        buildFromParams(mapper, builder, datasetSetters, datasetInfo);
+        if(!locationInfo.isEmpty()){
+            HashMap<String,HashMap<String,List<String>>> locationsEntries = new LinkedHashMap<>();
+            List<DatasetLocation> locations = new ArrayList<>();
+            for(String locParam: locationInfo.keySet()){ // locationInfo.keySet() is sorted
+                String[] keys = locParam.split( "\\.");
+                String locEntry = keys[0];
+                String field = keys[1];
+                if(!locationsEntries.containsKey( locEntry)){ // Will be sorted as well
+                    locationsEntries.put(locEntry, new HashMap<String,List<String>>());
+                }
+                locationsEntries.get(locEntry).put(field, formParams.get(locParam));
+            }
+            for(HashMap<String,List<String>> loc: locationsEntries.values()){
+                DatasetLocation.Builder dsb = new DatasetLocation.Builder();
+                buildFromParams(mapper, dsb, locationSetters, loc);
+                locations.add(dsb.build()); // Again, still sorted
+            }
+            if(locations.size() == 1){
+                builder.location(locations.get(0));
+            } else {
+                builder.locations(locations);
+            }
+        }
         return builder;
     }
-    
-    private static void buildFromParams(ObjectMapper mapper, Object builder, HashMap<String, Method> setters, MultivaluedMap<String,String> formParams){
+
+    private static void buildFromParams(ObjectMapper mapper, DatacatObject.Builder builder, HashMap<String, Method> setters, Map<String,List<String>> formParams){
+        String key = null;
         try {
-            for(String key: formParams.keySet()){
+            for(Iterator<String> iter = formParams.keySet().iterator(); iter.hasNext();){
+                key = iter.next();
                 Method m = setters.get( key );
-                List<String> lValue = formParams.get( key );
-                if(lValue.size() != 1){
-                    throw new RuntimeException( "Only one value per parameter is supported" );
+                if(m == null){
+                    throw new IllegalArgumentException("Unable to set field: " + key);
                 }
-                Class<?> targetValueType = m.getParameterTypes()[0]; // Should only be one
-                Object value = mapper.convertValue( lValue.get( 0 ), targetValueType );
+                List<String> lValue = formParams.get( key );
+                Object value = null;
+                if(key.equals( "metadata")){
+                    value = processMetadata(lValue);
+                } else {
+                    if(lValue.size() != 1){
+                        throw new RuntimeException( "Only one value per parameter is supported" );
+                    }
+                    Class<?> targetValueType = m.getParameterTypes()[0]; // Should only be one
+                    value = mapper.convertValue( lValue.get( 0 ), targetValueType );
+                }
                 m.invoke( builder, value );
             }
         } catch(IllegalAccessException | IllegalArgumentException | InvocationTargetException ex) { 
-            throw new RuntimeException("Error attempting to build object. Check builder @JsonSetter annotations", ex);
+            throw new IllegalArgumentException("Error attempting to build object. Check builder @JsonSetter annotations: " + key, ex);
+        }
+    }
+    
+    private static List<MetadataEntry> processMetadata(List<String> mdEntries) throws RuntimeException{
+        ObjectMapper mdMapper = new ObjectMapper();
+        AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
+        AnnotationIntrospector secondary = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+        AnnotationIntrospector pair = new AnnotationIntrospectorPair(primary, secondary);
+        mdMapper.setAnnotationIntrospector( pair );
+        TypeReference<List<MetadataEntry>> compoundRef = new TypeReference<List<MetadataEntry>>(){};
+        try {
+            ArrayList<MetadataEntry> metadata = new ArrayList<>();
+            for(String mdString: mdEntries){
+                List<MetadataEntry> mdList = mdMapper.readValue(mdString, compoundRef);
+                metadata.addAll(mdList);
+            }
+            return metadata;
+        } catch (IOException ex){
+            throw new RuntimeException("Unable to deserialize metadata", ex);
         }
     }
 }
---
+++ rest/src/test/java/org/srs/datacat/rest/FormParamConverterTest.java
@@ -0,0 +1,155 @@
+
+package org.srs.datacat.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.AnnotationIntrospector;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.introspect.AnnotationIntrospectorPair;
+import com.fasterxml.jackson.databind.introspect.JacksonAnnotationIntrospector;
+import com.fasterxml.jackson.databind.type.TypeFactory;
+import com.fasterxml.jackson.module.jaxb.JaxbAnnotationIntrospector;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import junit.framework.TestCase;
+import org.junit.Test;
+import org.srs.datacat.model.DatasetView;
+import org.srs.datacat.shared.Dataset;
+import org.srs.datacat.shared.DatasetLocation;
+import org.srs.datacat.shared.dataset.FlatDataset;
+import org.srs.datacat.shared.dataset.FullDataset;
+import org.srs.datacat.test.HSqlDbHarness;
+import org.srs.rest.shared.metadata.MetadataEntry;
+
+
+/**
+ *
+ * @author bvan
+ */
+public class FormParamConverterTest {
+    ObjectMapper mdMapper = new ObjectMapper();
+    
+    public FormParamConverterTest(){
+        AnnotationIntrospector primary = new JacksonAnnotationIntrospector();
+        AnnotationIntrospector secondary = new JaxbAnnotationIntrospector(TypeFactory.defaultInstance());
+        AnnotationIntrospector pair = new AnnotationIntrospectorPair(primary, secondary);
+        mdMapper.setAnnotationIntrospector( pair );
+    }
+    
+    
+    private HashMap<String, List<String>> startDataset(String name){
+        HashMap<String,List<String>> flatMap = new HashMap<>();
+        flatMap.put( "name", Arrays.asList(name));
+        flatMap.put( "dataType", Arrays.asList(HSqlDbHarness.JUNIT_DATASET_DATATYPE));
+        flatMap.put( "datasetSource", Arrays.asList(HSqlDbHarness.JUNIT_DATASET_DATASOURCE));
+        flatMap.put( "fileFormat", Arrays.asList(HSqlDbHarness.JUNIT_DATASET_FILEFORMAT));
+        flatMap.put( "versionId", Arrays.asList(Integer.toString(DatasetView.NEW_VER)));
+        HashMap<String, Object> metadata = new HashMap<>();
+        metadata.put( "num", 1234);
+        metadata.put( "alpha", "abc");
+        String md = "";
+        try {
+            md = mdMapper.writeValueAsString(MetadataEntry.toList( metadata ));
+            System.out.println(md);
+        } catch(JsonProcessingException ex) { }
+        flatMap.put("versionMetadata", Arrays.asList(md));
+        return flatMap;
+    }
+    
+    @Test
+    public void testFlatDataset(){
+        HashMap<String, List<String>> dsMap = null;
+        Dataset.Builder b = null;
+        FlatDataset ds = null;
+        dsMap = startDataset("hello");
+        b = FormParamConverter.getDatasetBuilder( dsMap );
+        ds = (FlatDataset) b.build();
+        TestCase.assertEquals("hello", ds.getName());
+        TestCase.assertEquals(HSqlDbHarness.JUNIT_DATASET_DATASOURCE, ds.getDatasetSource());
+        System.out.println(ds.isMaster());
+        
+        dsMap = startDataset("test2");
+        dsMap.put("resource", Arrays.asList("file:///afs/slac/fakeFile.txt"));
+        dsMap.put("site", Arrays.asList("SLAC"));
+        b = FormParamConverter.getDatasetBuilder( dsMap );
+        ds = (FlatDataset) b.build();
+        TestCase.assertEquals("test2", ds.getName());
+        TestCase.assertEquals("file:///afs/slac/fakeFile.txt", ds.getResource());
+        TestCase.assertEquals("SLAC", ds.getSite());
+        System.out.println(ds.isMaster());
+        
+        String locationId = null;
+        locationId = "location1.";
+        dsMap = startDataset("test3");
+        dsMap.put(locationId + "resource", Arrays.asList("file:///afs/slac/fakeFile.txt"));
+        dsMap.put(locationId + "site", Arrays.asList("SLAC"));
+        dsMap.put(locationId + "master", Arrays.asList("true"));
+        b = FormParamConverter.getDatasetBuilder( dsMap );
+        ds = (FlatDataset) b.build();
+        TestCase.assertEquals("test3", ds.getName());
+        TestCase.assertEquals("file:///afs/slac/fakeFile.txt", ds.getResource());
+        TestCase.assertEquals("SLAC", ds.getSite());
+        TestCase.assertTrue(ds.isMaster());
+        
+    }
+    
+    public void testFullDataset(){
+        HashMap<String, List<String>> dsMap = null;
+        List<DatasetLocation> locations = null;
+        Dataset.Builder b = null;
+        FullDataset ds = null;
+        String locationId = null;
+        
+        // Ordered location
+        dsMap = startDataset("test1");
+        locationId = "location1.";
+        dsMap.put(locationId + "resource", Arrays.asList("file:///afs/slac/fakeFile1.txt"));
+        dsMap.put(locationId + "site", Arrays.asList("SLAC1"));
+        dsMap.put(locationId + "master", Arrays.asList("true"));
+        
+        locationId = "location2.";
+        dsMap.put(locationId + "resource", Arrays.asList("file:///afs/slac/fakeFile2.txt"));
+        dsMap.put(locationId + "site", Arrays.asList("SLAC2"));
+
+        b = FormParamConverter.getDatasetBuilder( dsMap );
+        ds = (FullDataset) b.build();
+        locations = new ArrayList<>(ds.getLocations());
+        
+        TestCase.assertEquals("test1", ds.getName());
+        
+        DatasetLocation loc1 = locations.get(0);
+        TestCase.assertEquals("file:///afs/slac/fakeFile1.txt", loc1.getResource());
+        TestCase.assertEquals("SLAC1", loc1.getSite());
+        TestCase.assertTrue(loc1.isMaster());
+        DatasetLocation loc2 = locations.get(1);
+        TestCase.assertEquals("file:///afs/slac/fakeFile2.txt", loc2.getResource());
+        TestCase.assertEquals("SLAC2", loc2.getSite());
+        
+        // Add the location in a different order (check sorting)
+        dsMap = startDataset("test2");
+        locationId = "location2.";
+        dsMap.put(locationId + "resource", Arrays.asList("file:///afs/slac/fakeFile2.txt"));
+        dsMap.put(locationId + "site", Arrays.asList("SLAC2"));
+        
+        locationId = "location1.";
+        dsMap.put(locationId + "resource", Arrays.asList("file:///afs/slac/fakeFile1.txt"));
+        dsMap.put(locationId + "site", Arrays.asList("SLAC1"));
+        dsMap.put(locationId + "master", Arrays.asList("true"));
+
+        b = FormParamConverter.getDatasetBuilder( dsMap );
+        ds = (FullDataset) b.build();
+        locations = new ArrayList<>(ds.getLocations());
+        
+        TestCase.assertEquals("test3", ds.getName());
+        loc1 = locations.get(0);
+        TestCase.assertEquals("file:///afs/slac/fakeFile1.txt", loc1.getResource());
+        TestCase.assertEquals("SLAC1", loc1.getSite());
+        TestCase.assertTrue(loc1.isMaster());
+        loc2 = locations.get(1);
+        TestCase.assertEquals("file:///afs/slac/fakeFile2.txt", loc2.getResource());
+        TestCase.assertEquals("SLAC2", loc2.getSite());
+    }
+}
