d58b2b86dfc62514f055ef473c0acf5ea040d006
Brian Van Klaveren <bvan@slac.stanford.edu>
2014-11-05 00:58:54 +0000

    Improve permissions model.

--- core/src/main/java/org/srs/datacat/vfs/security/DcPermissions.java
+++ core/src/main/java/org/srs/datacat/vfs/security/DcPermissions.java
@@ -9,10 +9,68 @@ import java.nio.file.attribute.AclEntryPermission;
  */
 public class DcPermissions {
 
-    public static final AclEntryPermission CREATE_CHILD = AclEntryPermission.ADD_FILE;
-    public static final AclEntryPermission DELETE = AclEntryPermission.DELETE;
+    /**
+     *  READ allows a user to lookup the contents of a directory and read the datasets in
+     *  in that directory. It does NOT necessarily allow the user to lookup the contents
+     *  of a subdirectory
+     */
     public static final AclEntryPermission READ = AclEntryPermission.READ_DATA;
-    public static final AclEntryPermission MODIFY = AclEntryPermission.WRITE_DATA;
+    
+    /**
+     * INSERT allows a user to create a dataset (and add versions/locations) or subdirectory.
+     * It DOES NOT imply that a user will be able to overwrite a dataset, version,
+     * locations, or modify or add metadata.
+     */
+    public static final AclEntryPermission INSERT = AclEntryPermission.APPEND_DATA;
+    
+    /**
+     * DELETE allows a user to delete datasets from a directory or the directory itself.
+     * It does not necessarily apply to the subdirectories. If a user does not have permission
+     * to delete the subdirectories, the user will not have permission to delete the directory.
+     */
+    public static final AclEntryPermission DELETE = AclEntryPermission.DELETE;
+    
+    /**
+     * MODIFY allows a user to modify this directory, including it's name and metadata,
+     * as well as modify datasets, versions, locations, and their metadata. It does not 
+     * allow a user to delete a dataset, but a user may be able to delete versions and locations.
+     * MODIFY is necessary for dataset view merging, as a MERGE will remove the current version.
+     */
+    public static final AclEntryPermission WRITE = AclEntryPermission.WRITE_DATA;
+    
+    /**
+     * ADMIN allows a user in a group to administer the ACLs.
+     */
     public static final AclEntryPermission ADMIN = AclEntryPermission.WRITE_ACL;
     
+    public static String pack(AclEntryPermission permission){
+        if(permission == READ)
+            return "r";
+        if(permission == INSERT)
+            return "i";
+        if(permission == DELETE)
+            return "d";
+        if(permission == WRITE)
+            return "w";
+        if(permission == ADMIN)
+            return "a";
+        return "";
+    }
+    
+    public static AclEntryPermission unpack(String permission){
+        switch(permission){
+            case "r":
+                return READ;
+            case "i":
+                return INSERT;
+            case "d":
+                return DELETE;
+            case "w":
+                return WRITE;
+            case "a":
+                return ADMIN;
+        }
+        throw new IllegalArgumentException("Unable to unpack permission");
+    }
+
 }
