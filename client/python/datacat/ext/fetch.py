2abfa9de4bbd9b6f457cea4c6c5752622e100a7f
Brian Van Klaveren <bvan@slac.stanford.edu>
2015-02-10 01:02:54 +0000

    Support file name template

--- client/python/datacat/ext/fetch.py
+++ client/python/datacat/ext/fetch.py
@@ -4,23 +4,31 @@ import gevent.monkey
 gevent.monkey.patch_socket()
 
 from gevent.queue import Queue
-
 import requests
-import logging
-logging.basicConfig(level=logging.DEBUG)
+from datacat.model import unpack, pack
+import json
+from jinja2 import Template
+
+default_template = Template("{{ name }}.{{ fileFormat}}")
 
 q = Queue()
-for i in requests.get("http://scalnx-v04.slac.stanford.edu:8180/org-srs-webapps-datacat-0.2-SNAPSHOT/r/search.json/LSST/mirror/BNL3/workarea/ccdtest/e2v/113-03/flat/20140709-112014?max=10").json():
+
+search_url = "http://scalnx-v04.slac.stanford.edu:8180/org-srs-webapps-datacat-0.2-SNAPSHOT/r/search.json/LSST/mirror/BNL3/workarea/ccdtest/e2v/113-03/flat/20140709-112014?max=10"
+
+resp = requests.get(search_url)
+for i in unpack(resp.content):
     q.put(i)
 
 dl_url = "http://srs.slac.stanford.edu/DataCatalog/get?dataset=%d&datasetVersion=%d&datasetLocation=%d"
 
 def worker(thread_id):
     while not q.empty():
-        work_item = q.get()
-        ds = work_item["pk"]
-        ver = work_item["versionPk"]
-        loc = work_item["locations"][0]["pk"]
+        dataset = q.get()
+        ds = dataset.pk
+        ver = dataset.versionPk
+        loc = dataset.locations[0].pk
+        # Roundabout way to create an environment for the template
+        print(default_template.render(json.loads(pack(dataset))))
         response = requests.get(dl_url %(ds, ver, loc))
         for chunk in response.iter_content(8192):
             pass
