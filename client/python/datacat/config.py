aa3cc260e8552915d825b04115ed06a64570cc36
Brian Van Klaveren <bvan@slac.stanford.edu>
2015-02-13 21:25:20 +0000

    Fix config

--- client/python/datacat/config.py
+++ client/python/datacat/config.py
@@ -7,7 +7,7 @@ INSTALLATIONS = "srs fermi".split(" ")
 
 DEV_SRS_URL = "http://scalnx-v04.slac.stanford.edu:8180/datacat-v%s-SNAPSHOT/r" %(__version__)
 PROD_SRS_URL = "http://srs.slac.stanford.edu/datacat-v%s/r" %(__version__)
-DEV_LSST_DM_URL = "http://lsst-db2.slac.stanford.edu/datacat-v%s-SNAPSHOT/r" %(__version__)
+DEV_LSST_DM_URL = "http://lsst-db2.slac.stanford.edu:8180/datacat-v%s-SNAPSHOT/r" %(__version__)
 #DEV_FERMI_URL
 
 SRS_EXPERIMENTS = "srs exo cdms lsst lsst-desc lsst-camera".split(" ")
@@ -16,8 +16,11 @@ LSST_DM_EXPERIMENT = "lsst-dm"
 def CONFIG_URL(experiment, mode="dev"):
     experiment = experiment or "srs"
     is_srs = experiment.lower() in SRS_EXPERIMENTS
-    if mode is "dev" and is_srs:
-        return DEV_SRS_URL
+    if is_srs:
+        if mode == "dev":
+            return DEV_SRS_URL
+        elif mode == "prod":
+            return PROD_SRS_URL
     if experiment == LSST_DM_EXPERIMENT:
         return DEV_LSST_DM_URL
     return None
