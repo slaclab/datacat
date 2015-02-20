29966a088e83e90576e54049215740b2eebea704
Brian Van Klaveren <bvan@slac.stanford.edu>
2015-02-20 02:17:51 +0000

    Move HTTP-specific logic to new http_client module. Rename methods.

--- client/python/datacat/client.py
+++ client/python/datacat/client.py
@@ -1,10 +1,7 @@
 
 import os
-import requests
-import urllib
-from config import ENDPOINTS, DATATYPES
 from .model import *
-
+from .http_client import HttpClient
 
 class DcException(Exception):
     def __init__(self, resp):
@@ -26,60 +23,33 @@ class DcException(Exception):
 class Client(object):
 
     '''
-    HTTP-level abstraction over
+    Pythonic Client for interacting with the data catalog. This client interacts solely through JSON.
     '''
 
-    ALLOWABLE_VERSIONS = "curr current latest new next".split(" ")
-    ALLOWABLE_SITES = "master canonical all".split(" ")
-
     def __init__(self, base_url, *args, **kwargs):
+        self.http_client = HttpClient(base_url, *args, **kwargs)
         self.base_url = base_url
-    
-    def children(self, path, versionId=None, site=None, offset=None, max_num=None, accept="json", **kwargs):
-        endpoint = "path"
-        param_list = "offset:offset max_num:max".split(" ")
-        param_map = dict([i.split(":") for i in param_list])
-        params = {param_map[k]:v for k,v in locals().items() if k in param_map and v is not None}
-        target = self._target(endpoint, path, versionId, site, accept) + ";children"
-        return self._req("get", target, params, **kwargs)
-    
-    def path(self, path, versionId=None, site=None, accept="json", **kwargs):
-        endpoint = "path"
-        return self._req("get", self._target(endpoint, path, versionId, site, accept), **kwargs)
+
+    def path(self, path, versionId=None, site=None, **kwargs):
+        resp = self.http_client.path(path, versionId, site, accept="json", **kwargs)
+        self._check_response(resp)
+        return unpack(resp.content)
+
+    def children(self, path, versionId=None, site=None, offset=None, max_num=None, **kwargs):
+        resp = self.http_client.children(path, versionId, site, offset, max_num, accept="json", **kwargs)
+        self._check_response(resp)
+        return unpack(resp.content)
 
     def exists(self, path, versionId=None, site=None, **kwargs):
-        endpoint = "path"
         try:
-            resp = self._req("get", self._target(endpoint, path, versionId, site), accept="json", **kwargs)
+            self.path(path, versionId, site)
             return True
         except DcException as e:
             if e.raw.status_code > 404 or e.raw.status_code == 400:
                 raise e
         return False
 
-    def mkfolder(self, path, parents=False, metadata=None, folderExtras=None, **kwargs):
-        """
-        Make a new Folder
-        :param path: Container Target path
-        :param parents: If true, will create intermediate Folders as required.
-        :param metadata: Metadata to add to when creating folder
-        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
-        The object will be a Folder
-        """
-        return self.mkdir(path, type="folder", parents=parents, metadata=metadata, extras=folderExtras, **kwargs)
-
-    def mkgroup(self, path, parents=False, metadata=None, groupExtras=None, **kwargs):
-        """
-        Make a new Folder
-        :param path: Container Target path
-        :param parents: If true, will create intermediate Folders as required.
-        :param metadata: Metadata to add to when creating folder
-        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
-        The object will be a Folder
-        """
-        return self.mkdir(path, type="group", parents=parents, metadata=metadata, extras=groupExtras, **kwargs)
-
-    def mkdir(self, path, type="folder", parents=False, metadata=None, extras=None, **kwargs):
+    def mkdir(self, path, type="folder", parents=False, metadata=None, **kwargs):
         """
         Make a new Container
         :param path: Container Target path
@@ -91,14 +61,9 @@ class Client(object):
         """
         parentpath = os.path.dirname(path)
         if type.lower() == "folder":
-            endpoint = "folders"
             container = Folder(path=path, name=path.split("/")[-1], metadata=metadata)
         elif type.lower() == "group":
-            endpoint = "groups"
             container = Group(path=path, name=path.split("/")[-1], metadata=metadata)
-        headers = kwargs.get("headers", {})
-        headers["content-type"] = "application/json"
-        kwargs["headers"] = headers
         if parents:
             parts = []
             while not self.exists(parentpath):
@@ -108,23 +73,35 @@ class Client(object):
                 for part in parts:
                     parentpath = os.path.join(parentpath, parts.pop())
                     self.mkdir(parentpath)
-        return self._req("post",self._target(endpoint, parentpath), data=pack(container), **kwargs)
+        headers = kwargs.get("headers", {})
+        headers["content-type"] = "application/json"
+        kwargs["headers"] = headers
+        resp = self.http_client.mkdir(path, type, payload=pack(container), **kwargs)
+        self._check_response(resp)
+        return unpack(resp.content)
 
-    def rmdir(self, path, type="folder", **kwargs):
+    def mkfolder(self, path, parents=False, metadata=None, **kwargs):
         """
-        Remove a container.
-        :param path: Path of container to remove.
-        :param type: Type of container (Group or Folder). This will be removed in a future version.
-        :return: A :class`requests.Response` object. A client can inspect the status code.
+        Make a new Folder
+        :param path: Container Target path
+        :param parents: If true, will create intermediate Folders as required.
+        :param metadata: Metadata to add to when creating folder
         """
-        if type.lower() == "folder":
-            endpoint = "folders"
-        elif type.lower() == "group":
-            endpoint = "groups"
-        return self._req("delete",self._target(endpoint, path), **kwargs)
+        return self.mkdir(path, "folder", parents, metadata, **kwargs)
+
+    def mkgroup(self, path, parents=False, metadata=None, **kwargs):
+        """
+        Make a new Folder
+        :param path: Container Target path
+        :param parents: If true, will create intermediate Folders as required.
+        :param metadata: Metadata to add to when creating folder
+        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
+        The object will be a Folder
+        """
+        return self.mkdir(path, "group", parents, metadata, **kwargs)
 
     def mkds(self, path, name, dataType, fileFormat, versionId="new", site=None, versionMetadata=None,
-             resource=None, **kwargs):
+             resource=None, datasetExtras=None, versionExtras=None, locationExtras=None, **kwargs):
         """
         Make a dataset.
         :param path: Container Target path
@@ -135,62 +112,8 @@ class Client(object):
         :param site: Site where the dataset physically resides (i.e. SLAC, IN2P3)
         :param versionMetadata: Metadata to add to registered version if registering a version.
         :param resource: The actual file resource path at the given site (i.e. /nfs/farm/g/glast/dataset.dat)
-        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
-        The object will be a Dataset.
+        :return: A representation of the dataset that was just created.
         """
-        return self.create_dataset(path, name, dataType, fileFormat, versionId, site, versionMetadata, resource)
-
-    def rmds(self, path, **kwargs):
-        """
-        Remove a dataset.
-        :param path: Path of dataset to remove
-        :param kwargs:
-        :return: A :class`requests.Response` object. A client can inspect the status code.
-        """
-        return self.delete_dataset(path, **kwargs)
-
-    def search(self, target, versionId=None, site=None, query=None, sort=None, show=None, offset=None, max_num=None,
-               accept="json", **kwargs):
-        """Search a target. A target is a Container of some sort. It may also be specified as a glob, as in:
-         1. /path/to - target /path/to _only_
-         2. /path/to/* - target is all containers directly in /path/to/
-         3. /path/to/** - target is all containers, recursively, under /path/to/
-         4. /path/to/*$ - target is only folders directly under /path/to/
-         5. /path/to/**^ - target is only groups, recursively, under /path/to/
-
-        :param target: The path (or glob-like path) of which to search
-        :param query: The query
-        :param sort: Fields and Metadata fields to sort on.
-        :param show: Metadata fields to optionally return
-        :param offset: Offset at which to start returning objects.
-        :param max_num: Maximum number of objects to return.
-        :param accept: Format of the response object which is returned.
-        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
-        The object will be a collection.
-        """
-        endpoint = "search"
-        param_list = "query:filter sort:sort show:show offset:offset max_num:max".split(" ")
-        param_map = dict([i.split(":") for i in param_list])
-        params = {param_map[k]:v for k,v in locals().items() if k in param_map and v is not None}
-        return self._req("get",self._target(endpoint, target, versionId, site, accept), params, **kwargs)
-
-    def create_dataset(self, path, name, dataType, fileFormat,
-                       versionId="new", site=None, versionMetadata=None, resource=None,
-                       datasetExtras=None, versionExtras=None, locationExtras=None,
-                       **kwargs):
-        """
-        :param path: Container Target path
-        :param name: Name of Dataset you wish to create
-        :param dataType: User-Defined Data Type of Dataset. This is often a subtype of a file format.
-        :param fileFormat: The File Format of the Dataset (i.e. root, fits, tar.gz, txt, etc...)
-        :param versionId: Desired versionId. By default, it is set to "new", which will result in a versionId of 0.
-        :param site: Site where the dataset physically resides (i.e. SLAC, IN2P3)
-        :param versionMetadata: Metadata to add to registered version if registering a version.
-        :param resource: The actual file resource path at the given site (i.e. /nfs/farm/g/glast/dataset.dat)
-        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
-        The object will be a Dataset.
-        """
-        endpoint = "datasets"
         has_version = versionId is not None
         has_location = site is not None and resource is not None
         if not has_version and has_location:
@@ -210,9 +133,44 @@ class Client(object):
         headers = kwargs.get("headers", {})
         headers["content-type"] = "application/json"
         kwargs["headers"] = headers
-        return self._req("post",self._target(endpoint, path), data=pack(ds), **kwargs)
+        resp = self.http_client.mkds(path, pack(ds), **kwargs)
+        self._check_response(resp)
+        return unpack(resp.content)
 
-    def patch_dataset(self, path, dataset, versionId="current", site=None, **kwargs):
+    def create_dataset(self, path, name, dataType, fileFormat,
+                       versionId="new", site=None, versionMetadata=None, resource=None,
+                       datasetExtras=None, versionExtras=None, locationExtras=None,
+                       **kwargs):
+        """
+        See mkds
+        """
+        return self.mkds(path, name, dataType, fileFormat, versionId, site, versionMetadata,
+                        resource, datasetExtras, versionExtras, locationExtras, **kwargs)
+
+    def rmdir(self, path, type="folder", **kwargs):
+        """
+        Remove a container.
+        :param path: Path of container to remove.
+        :param type: Type of container (Group or Folder). This will be removed in a future version.
+        :return: A :class`requests.Response` object. A client can inspect the status code.
+        """
+        resp = self.http_client.rmdir(path, type, **kwargs)
+        self._check_response(resp, 204)
+        return True
+
+    def rmds(self, path, **kwargs):
+        """
+        Remove a dataset.
+        :param path: Path of dataset to remove
+        :param kwargs:
+        """
+        resp = self.http_client.rmds(path, **kwargs)
+        self._check_response(resp, 204)
+
+    def delete_dataset(self, path, **kwargs):
+        return self.rmds(path, **kwargs)
+
+    def patchds(self, path, dataset, versionId="current", site=None, **kwargs):
         """
         Patch a dataset.
         :param path: Path of the dataset to patch.
@@ -224,15 +182,15 @@ class Client(object):
         :param kwargs:
         :return: A representation of the patched dataset
         """
-        endpoint = "datasets"
         headers = kwargs.get("headers", {})
         headers["content-type"] = "application/json"
         kwargs["headers"] = headers
-        req = dataset if type(dataset) == Dataset else Dataset(**dataset)
-        return self._req("patch",self._target(endpoint, path, versionId, site),
-                         data=pack(req), **kwargs)
+        ds = dataset if type(dataset) == Dataset else Dataset(**dataset)
+        resp = self.http_client.patchds(path, pack(ds), versionId, site, **kwargs)
+        self._check_response(resp)
+        return unpack(resp.content)
 
-    def patch_container(self, path, container, type="folder", **kwargs):
+    def patchdir(self, path, container, type="folder", **kwargs):
         """
         Patch a container.
         :param path: Path of the dataset to patch.
@@ -242,69 +200,50 @@ class Client(object):
         :param kwargs:
         :return: A representation of the patched dataset
         """
-        if type.lower() == "folder":
-            endpoint = "folders"
-        elif type.lower() == "group":
-            endpoint = "groups"
         headers = kwargs.get("headers", {})
         headers["content-type"] = "application/json"
         kwargs["headers"] = headers
+
         if isinstance(container, Container):
             req = container
         elif type == "group":
             req = Group(**container)
         elif type == "folder":
             req = Folder(**container)
-        return self._req("patch",self._target(endpoint, path), data=pack(req), **kwargs)
 
-    def delete_dataset(self, path, **kwargs):
-        endpoint = "datasets"
-        return self._req("delete",self._target(endpoint, path), **kwargs)
+        resp = self.http_client.patchdir(path, pack(container), type, **kwargs)
+        self._check_response(resp, 204)
+        return unpack(resp.content)
 
-    def _req(self, http_method, target, params=None, data=None, **kwargs):
-        headers = kwargs["headers"] if "headers" in kwargs else None
-        requests_method = getattr(requests, http_method)
-        resp = requests_method(target, params=params, headers=headers, data=data)
-        if kwargs.get('show_request', False):
-            print("Request")
-            if data:
-                print("Data:\n" + str(data))
-            print(resp.request.url)
-        if kwargs.get('request_headers', False):
-            print("Request Headers:")
-            print(resp.request.headers)
-        if kwargs.get('show_response', False):
-            print("Response: %d" %resp.status_code)
-        if kwargs.get('response_headers', False):
-            print("Response Headers:")
-            print(resp.headers)
-        if kwargs.get('show_raw_response', False):
-            print(kwargs["show_raw_response"])
-            print("Response:")
-            print(resp.content)
-        if resp.status_code >= 300:
-            raise DcException(resp)
-        return resp
+    def patch_dataset(self, path, dataset, versionId="current", site=None, **kwargs):
+        return self.patchds(path, dataset, versionId, site, **kwargs)
 
-    def _target(self, endpoint, path, version=None, site=None, accept="json"):
-        if version is not None:
-            try:
-                version = int(version)
-            except ValueError as e:
-                if version.lower() in self.ALLOWABLE_VERSIONS:
-                    version = version.lower()
-                else:
-                    raise e
+    def patch_container(self, path, container, type="folder", **kwargs):
+        return self.patchdir(path, container, type, **kwargs)
 
-        def resource(endpoint, accept="json"):
-            if endpoint in ENDPOINTS and accept in DATATYPES:
-                return "%s.%s" %(endpoint, accept)
-        def resolve(path, part):
-            path = path if path[-1] != '/' else path[:-1]
-            part = part if part[0] != '/' else (part[1:] if len(part) > 0 else "")
-            return "%s/%s" %(path, urllib.quote(part))
-        url = resolve(self.base_url, resource(endpoint, accept))
-        view = ";v=" + str(version) if version is not None else ""
-        view += ";s=" + site if site is not None else ""
-        return resolve(url, path) + view
+    def search(self, target, versionId=None, site=None, query=None, sort=None, show=None, offset=None, max_num=None,
+               **kwargs):
+        """Search a target. A target is a Container of some sort. It may also be specified as a glob, as in:
+         1. /path/to - target /path/to _only_
+         2. /path/to/* - target is all containers directly in /path/to/
+         3. /path/to/** - target is all containers, recursively, under /path/to/
+         4. /path/to/*$ - target is only folders directly under /path/to/
+         5. /path/to/**^ - target is only groups, recursively, under /path/to/
 
+        :param target: The path (or glob-like path) of which to search
+        :param query: The query
+        :param sort: Fields and Metadata fields to sort on.
+        :param show: Metadata fields to optionally return
+        :param offset: Offset at which to start returning objects.
+        :param max_num: Maximum number of objects to return.
+        :param accept: Format of the response object which is returned.
+        """
+        resp = self.http_client.search(target, versionId, site, query, sort, show, offset, max_num, **kwargs)
+        self._check_response(resp)
+        return unpack(resp.content)
+
+    def _check_response(self, response, expected_status=None):
+        if response.status_code >= 300:
+            raise DcException(response)
+        if expected_status and response.status_code != expected_status:
+            raise DcException()
---
+++ client/python/datacat/http_client.py
@@ -0,0 +1,188 @@
+
+import os
+import requests
+import urllib
+from config import ENDPOINTS, DATATYPES
+
+class HttpClient(object):
+
+    '''
+    HTTP-level abstraction over the RESTful Client.
+    '''
+
+    ALLOWABLE_VERSIONS = "curr current latest new next".split(" ")
+    ALLOWABLE_SITES = "master canonical all".split(" ")
+
+    def __init__(self, base_url, *args, **kwargs):
+        self.base_url = base_url
+    
+    def children(self, path, versionId=None, site=None, offset=None, max_num=None, accept="json", **kwargs):
+        endpoint = "path"
+        param_list = "offset:offset max_num:max".split(" ")
+        param_map = dict([i.split(":") for i in param_list])
+        params = {param_map[k]:v for k,v in locals().items() if k in param_map and v is not None}
+        target = self._target(endpoint, path, versionId, site, accept) + ";children"
+        return self._req("get", target, params, **kwargs)
+    
+    def path(self, path, versionId=None, site=None, accept="json", **kwargs):
+        endpoint = "path"
+        return self._req("get", self._target(endpoint, path, versionId, site, accept), **kwargs)
+
+    def mkdir(self, path, type="folder", payload=None, **kwargs):
+        """
+        Make a new Container
+        :param path: Container Target path
+        :param type: Container type. Defaults to folder.
+        :param parents: If true, will create intermediate Folders as required.
+        :param metadata: Metadata to add to when creating folder
+        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
+        The object will be a Folder
+        """
+        parentpath = os.path.dirname(path)
+        if type.lower() == "folder":
+            endpoint = "folders"
+        elif type.lower() == "group":
+            endpoint = "groups"
+        return self._req("post",self._target(endpoint, parentpath), data=payload, **kwargs)
+
+    def mkds(self, path, payload, **kwargs):
+        """
+        Make a dataset.
+        :param path: Container Target path
+        :param name: Name of Dataset you wish to create
+        :param dataType: User-Defined Data Type of Dataset. This is often a subtype of a file format.
+        :param fileFormat: The File Format of the Dataset (i.e. root, fits, tar.gz, txt, etc...)
+        :param versionId: Desired versionId. By default, it is set to "new", which will result in a versionId of 0.
+        :param site: Site where the dataset physically resides (i.e. SLAC, IN2P3)
+        :param versionMetadata: Metadata to add to registered version if registering a version.
+        :param resource: The actual file resource path at the given site (i.e. /nfs/farm/g/glast/dataset.dat)
+        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
+        The object will be a Dataset.
+        """
+        endpoint = "datasets"
+        return self._req("post",self._target(endpoint, path), data=payload, **kwargs)
+
+    def rmdir(self, path, type="folder", **kwargs):
+        """
+        Remove a container.
+        :param path: Path of container to remove.
+        :param type: Type of container (Group or Folder). This will be removed in a future version.
+        :return: A :class`requests.Response` object. A client can inspect the status code.
+        """
+        if type.lower() == "folder":
+            endpoint = "folders"
+        elif type.lower() == "group":
+            endpoint = "groups"
+        return self._req("delete",self._target(endpoint, path), **kwargs)
+
+    def rmds(self, path, **kwargs):
+        """
+        Remove a dataset.
+        :param path: Path of dataset to remove
+        :param kwargs:
+        :return: A :class`requests.Response` object. A client can inspect the status code.
+        """
+        endpoint = "datasets"
+        return self._req("delete",self._target(endpoint, path), **kwargs)
+
+    def patchds(self, path, payload, versionId="current", site=None, **kwargs):
+        """
+        Patch a dataset.
+        :param path: Path of the dataset to patch.
+        :param dataset: A dict object or a dataset.model.Dataset object representing the changes to be applied to the
+        dataset
+        :param versionId: If specified, identifies the version to patch. Otherwise, it's assumed to patch the current
+        version, should it exist.
+        :param site: If specified, identifies the specific location to be patched (i.e. SLAC, IN2P3)
+        :param kwargs:
+        :return: A representation of the patched dataset
+        """
+        endpoint = "datasets"
+        return self._req("patch",self._target(endpoint, path, versionId, site), data=payload, **kwargs)
+
+    def patchdir(self, path, payload, type="folder", **kwargs):
+        """
+        Patch a container.
+        :param path: Path of the dataset to patch.
+        :param type: Container type. Defaults to folder.
+        :param container: A dict object or a dataset.model.Group/Folder object representing the changes to be applied to the
+        container.
+        :param kwargs:
+        :return: A representation of the patched dataset
+        """
+        if type.lower() == "folder":
+            endpoint = "folders"
+        elif type.lower() == "group":
+            endpoint = "groups"
+        return self._req("patch",self._target(endpoint, path), data=payload, **kwargs)
+
+    def search(self, target, versionId=None, site=None, query=None, sort=None, show=None, offset=None, max_num=None,
+               accept="json", **kwargs):
+        """Search a target. A target is a Container of some sort. It may also be specified as a glob, as in:
+         1. /path/to - target /path/to _only_
+         2. /path/to/* - target is all containers directly in /path/to/
+         3. /path/to/** - target is all containers, recursively, under /path/to/
+         4. /path/to/*$ - target is only folders directly under /path/to/
+         5. /path/to/**^ - target is only groups, recursively, under /path/to/
+
+        :param target: The path (or glob-like path) of which to search
+        :param query: The query
+        :param sort: Fields and Metadata fields to sort on.
+        :param show: Metadata fields to optionally return
+        :param offset: Offset at which to start returning objects.
+        :param max_num: Maximum number of objects to return.
+        :param accept: Format of the response object which is returned.
+        :return: A :class`requests.Response` object. A user can use Response.content to get the content.
+        The object will be a collection.
+        """
+        endpoint = "search"
+        param_list = "query:filter sort:sort show:show offset:offset max_num:max".split(" ")
+        param_map = dict([i.split(":") for i in param_list])
+        params = {param_map[k]:v for k,v in locals().items() if k in param_map and v is not None}
+        return self._req("get",self._target(endpoint, target, versionId, site, accept), params, **kwargs)
+
+    def _req(self, http_method, target, params=None, data=None, **kwargs):
+        headers = kwargs["headers"] if "headers" in kwargs else None
+        requests_method = getattr(requests, http_method)
+        resp = requests_method(target, params=params, headers=headers, data=data)
+        if kwargs.get('show_request', False):
+            print("Request")
+            if data:
+                print("Data:\n" + str(data))
+            print(resp.request.url)
+        if kwargs.get('request_headers', False):
+            print("Request Headers:")
+            print(resp.request.headers)
+        if kwargs.get('show_response', False):
+            print("Response: %d" %resp.status_code)
+        if kwargs.get('response_headers', False):
+            print("Response Headers:")
+            print(resp.headers)
+        if kwargs.get('show_raw_response', False):
+            print(kwargs["show_raw_response"])
+            print("Response:")
+            print(resp.content)
+        return resp
+
+    def _target(self, endpoint, path, version=None, site=None, accept="json"):
+        if version is not None:
+            try:
+                version = int(version)
+            except ValueError as e:
+                if version.lower() in self.ALLOWABLE_VERSIONS:
+                    version = version.lower()
+                else:
+                    raise e
+
+        def resource(endpoint, accept="json"):
+            if endpoint in ENDPOINTS and accept in DATATYPES:
+                return "%s.%s" %(endpoint, accept)
+        def resolve(path, part):
+            path = path if path[-1] != '/' else path[:-1]
+            part = part if part[0] != '/' else (part[1:] if len(part) > 0 else "")
+            return "%s/%s" %(path, urllib.quote(part))
+        url = resolve(self.base_url, resource(endpoint, accept))
+        view = ";v=" + str(version) if version is not None else ""
+        view += ";s=" + site if site is not None else ""
+        return resolve(url, path) + view
+
