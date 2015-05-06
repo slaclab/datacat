66bce7d1cbc27c8b1a288b0735a6a3f89a893bf7
Brian Van Klaveren <bvan@slac.stanford.edu>
2015-05-06 18:19:36 +0000

    Don't import model directly into module

--- client/python/datacat/__init__.py
+++ client/python/datacat/__init__.py
@@ -2,5 +2,4 @@
 __version__ = "0.3-SNAPSHOT"
 
 from .client import Client, client_from_config, client_from_config_file
-from .model import DatacatNode, Dataset, Folder, Container
 from .config import auth_from_config, config_from_file, default_config
