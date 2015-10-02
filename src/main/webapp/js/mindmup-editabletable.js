4e726d1d1683fe4ac97f6ecc8bea72c8e06106d0
Brian Van Klaveren <bvan@slac.stanford.edu>
2015-10-02 01:16:34 +0000

    Use default mindmup table

--- src/main/webapp/js/mindmup-editabletable.js
+++ src/main/webapp/js/mindmup-editabletable.js
@@ -8,7 +8,7 @@ $.fn.editableTableWidget = function (options) {
 				return opts;
 			},
 			activeOptions = $.extend(buildDefaultOptions(), options),
-			ENTER = 13, ESC = 27, TAB = 9,
+			ARROW_LEFT = 37, ARROW_UP = 38, ARROW_RIGHT = 39, ARROW_DOWN = 40, ENTER = 13, ESC = 27, TAB = 9,
 			element = $(this),
 			editor = activeOptions.editor.css('position', 'absolute').hide().appendTo(element.parent()),
 			active,
@@ -40,15 +40,24 @@ $.fn.editableTableWidget = function (options) {
 				if (evt.result === false) {
 					active.html(originalContent);
 				}
+			},
+			movement = function (element, keycode) {
+				if (keycode === ARROW_RIGHT) {
+					return element.next('td');
+				} else if (keycode === ARROW_LEFT) {
+					return element.prev('td');
+				} else if (keycode === ARROW_UP) {
+					return element.parent().prev().children().eq(element.index());
+				} else if (keycode === ARROW_DOWN) {
+					return element.parent().next().children().eq(element.index());
+				}
+				return [];
 			};
 		editor.blur(function () {
 			setActiveText();
 			editor.hide();
-		})
-                .keydown(function (e) {
+		}).keydown(function (e) {
 			if (e.which === ENTER) {
-                                var evt = $.Event('validate');
-                                active.trigger(evt, editor.val());
 				setActiveText();
 				editor.hide();
 				active.focus();
@@ -62,13 +71,32 @@ $.fn.editableTableWidget = function (options) {
 				active.focus();
 			} else if (e.which === TAB) {
 				active.focus();
+			} else if (this.selectionEnd - this.selectionStart === this.value.length) {
+				var possibleMove = movement(active, e.which);
+				if (possibleMove.length > 0) {
+					possibleMove.focus();
+					e.preventDefault();
+					e.stopPropagation();
+				}
+			}
+		})
+		.on('input paste', function () {
+			var evt = $.Event('validate');
+			active.trigger(evt, editor.val());
+			if (evt.result === false) {
+				editor.addClass('error');
+			} else {
+				editor.removeClass('error');
 			}
 		});
 		element.on('click keypress dblclick', showEditor)
 		.css('cursor', 'pointer')
 		.keydown(function (e) {
-			var prevent = true;
-			if (e.which === ENTER) {
+			var prevent = true,
+				possibleMove = movement($(e.target), e.which);
+			if (possibleMove.length > 0) {
+				possibleMove.focus();
+			} else if (e.which === ENTER) {
 				showEditor(false);
 			} else if (e.which === 17 || e.which === 91 || e.which === 93) {
 				showEditor(true);
