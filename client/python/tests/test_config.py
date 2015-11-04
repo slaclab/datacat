ea4315fe1f5cd1f3d344b157c89e504be1ad7502
Brian Van Klaveren <bvan@slac.stanford.edu>
2015-11-04 21:55:43 +0000

    Implement unit tests

--- client/python/tests/test_config.py
+++ client/python/tests/test_config.py
@@ -3,35 +3,39 @@ import unittest
 from datacat.auth import HMACAuth, HMACAuthSRS
 from datacat import config_from_file, auth_from_config, client_from_config, client_from_config_file
 from datacat.config import default_config, default_url
+import os
+
+TEST_ROOT = os.path.dirname(__file__)
+
 
 class ConfigTest(unittest.TestCase):
 
     def test_srs_config_file(self):
-        config = config_from_file("config_srs.ini")
+        config = config_from_file(os.path.join(TEST_ROOT, "config_srs.ini"))
         auth = auth_from_config(config)
         self.assertEqual(type(auth), HMACAuthSRS)
 
     def test_srs_config_file_none(self):
-        config = config_from_file("config_srs.ini", "dev")
+        config = config_from_file(os.path.join(TEST_ROOT, "config_srs.ini"), "dev")
         print(config)
         auth = auth_from_config(config)
         self.assertEqual(auth, None)
 
     def test_hmac_config_file(self):
         _file = "config_hmac.ini"
-        config = config_from_file(_file)
+        config = config_from_file(os.path.join(TEST_ROOT, _file))
         auth = auth_from_config(config)
         self.assertEqual(type(auth), HMACAuth)
-        client = client_from_config_file(_file)
+        client = client_from_config_file(os.path.join(TEST_ROOT, _file))
         self.assertEqual(type(client.http_client.auth_strategy), HMACAuth)
 
     def test_no_config(self):
-        config = config_from_file()
+        config = config_from_file(path=os.path.join(TEST_ROOT, "config_blank.ini"))
         auth = auth_from_config(config)
         self.assertEqual(config["url"], default_url())
         self.assertEqual(auth, None)
 
-        client = client_from_config_file()
+        client = client_from_config_file(path=os.path.join(TEST_ROOT, "config_blank.ini"))
         self.assertEqual(client.http_client.auth_strategy, None)
 
     def test_default_config_with_mode(self):
@@ -45,10 +49,10 @@ class ConfigTest(unittest.TestCase):
         self.assertEqual(client.url, default_url(mode="prod"))
 
     def test_no_config_with_mode(self):
-        config = config_from_file(mode="prod")
+        config = config_from_file(os.path.join(TEST_ROOT, "config_blank.ini"), mode="prod")
         auth = auth_from_config(config)
         self.assertEqual(config["url"], default_url(mode="prod"))
         self.assertEqual(auth, None)
 
-        client = client_from_config_file()
+        client = client_from_config_file(path=os.path.join(TEST_ROOT, "config_blank.ini"))
         self.assertEqual(client.http_client.auth_strategy, None)
