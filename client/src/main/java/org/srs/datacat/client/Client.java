dca7904291c623770ee7db9797b5d16cbab01152
Brian Van Klaveren <bvan@slac.stanford.edu>
2015-12-09 21:18:51 +0000

    Add documentation and another search method

--- client/src/main/java/org/srs/datacat/client/Client.java
+++ client/src/main/java/org/srs/datacat/client/Client.java
@@ -302,6 +302,40 @@ public class Client {
         return resp.readEntity(new GenericType<DatasetModel>() {});
     }
 
+    /**
+     * Search a target. A target is a Container of some sort. It may also be specified as a glob, as in:
+     * @see #searchForDatasets(java.lang.String, java.lang.String, java.lang.String, java.lang.String, 
+     * java.lang.String[], java.lang.String[], java.lang.Integer, java.lang.Integer) 
+     * @param target The path (or glob-like path) of which to search
+     * @param versionId Version Id to return
+     * @param site Site to query
+     * @param query Query String
+     * @param sort Fields and Metadata fields to sort on.
+     * @param show Metadata fields to optionally return
+     * @return Response object of the search
+     */
+    public DatasetResultSetModel searchForDatasets(String target, String versionId, String site,
+            String query, String[] sort, String[] show){
+        return searchForDatasets(target, versionId, site, query, sort, show, null, null);
+    }
+    
+    /**
+     * Search a target. A target is a Container of some sort. It may also be specified as a glob, as in:
+     *   1. /path/to - target /path/to _only_
+     *   2. /path/to/* - target is all containers directly in /path/to/
+     *   3. /path/to/** - target is all containers, recursively, under /path/to/
+     *   4. /path/to/*$ - target is only folders directly under /path/to/
+     *   5. /path/to/**^ - target is only groups, recursively, under /path/to/
+     * @param target The path (or glob-like path) of which to search
+     * @param versionId Version Id to return
+     * @param site Site to query
+     * @param query Query String
+     * @param sort Fields and Metadata fields to sort on.
+     * @param show Metadata fields to optionally return
+     * @param offset Offset at which to start returning objects.
+     * @param max Maximum number of datasets to return
+     * @return Response object of the search
+     */
     public DatasetResultSetModel searchForDatasets(String target, String versionId, String site,
             String query, String[] sort, String[] show, Integer offset, Integer max){
         try {
