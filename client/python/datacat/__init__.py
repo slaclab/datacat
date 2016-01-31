aa78ad4267d975783030ff0c6c7e74a104b0e96b
Brian Van Klaveren <bvan@slac.stanford.edu>
2016-01-31 15:51:27 -0800

    Don't specify version

--- client/python/datacat/__init__.py
+++ client/python/datacat/__init__.py
@@ -1,6 +1,3 @@
-#
-__version__ = "0.4-RC1"
-
 from .auth import auth_from_config
 from .client import Client, client_from_config, client_from_config_file
 from .config import config_from_file, default_config
