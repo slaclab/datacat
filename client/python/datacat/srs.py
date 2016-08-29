3889389e5fd6a2caac170183dfd242f9be11c721
Brian Van Klaveren <bvan@slac.stanford.edu>
2016-08-29 12:02:41 -0700

    Fix handling of default url

--- client/python/datacat/srs.py
+++ client/python/datacat/srs.py
@@ -2,8 +2,8 @@ from .client import client_from_config
 from .config import config_from_file as cff
 from .config import __version__
 
-_DEV_SRS_URL = "http://srs.slac.stanford.edu/datacat-v%s-SNAPSHOT/r" % __version__
-_PROD_SRS_URL = "http://srs.slac.stanford.edu/datacat-v%s/r" % __version__
+_DEV_SRS_URL = "https://srs.slac.stanford.edu/datacat-v%s-SNAPSHOT/r" % __version__
+_PROD_SRS_URL = "https://srs.slac.stanford.edu/datacat-v%s/r" % __version__
 
 
 def config_from_file(path=None, override_section=None, mode=None, override_url=None):
@@ -17,8 +17,10 @@ def config_from_file(path=None, override_section=None, mode=None, override_url=N
     :return: Configured client
     :except: OSError if path is provided and the file doesn't exist.
     """
-    url = override_url or _default_url(mode)
-    return cff(path, override_section, override_url=url)
+    config = cff(path, override_section, override_url=override_url)
+    # Use a default URL if no URL was ever configured
+    config.setdefault('url', _default_url(mode))
+    return config
 
 
 def client_from_config_file(path=None, override_section=None):
